22030c7d4d523f856ad1d053b9e95e734ae28a6a
SnowSnail <f2961@yandex.ru>
2023-06-18 12:39:28 +0300

    Extend tailwind config with Twitter theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,40 @@ module.exports = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        twt: [
+          'var(--font-chirp)',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica',
+          'Arial',
+          'sans-serif',
+        ],
+      },
+      colors: {
+        twt: {
+          light: '#cfd9de',
+          DEFAULT: '#536471',
+          dark: '#0f1419',
+        },
+      },
+      width: {
+        'twt-img': '131px',
+      },
+      height: {
+        'twt-img': '130px',
+      },
+      transitionProperty: {
+        twt: 'background-color',
+      },
+      fontSize: {
+        twt: ['15px', '20px'],
       },
     },
   },
   plugins: [],
-}
+};
