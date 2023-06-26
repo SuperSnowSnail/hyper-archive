3873e4fe7302bdfd2003da280219ad194daa2cfd
SnowSnail <f2961@yandex.ru>
2023-06-26 19:20:07 +0300

    Remove mock data from categories container

--- components/CategoriesContainer.jsx
+++ components/CategoriesContainer.jsx
@@ -1,35 +1,26 @@
 import CategoryBtn from './CategoryBtn';
+import CategoryBtnSkeleton from './CategoryBtnSkeleton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
-const CategoriesContainer = ({ isLoading, activeId }) => {
+const CategoriesContainer = ({ isLoading, categories, activeId, setActiveId }) => {
   return isLoading ? (
     <div className='flex h-full w-full items-center justify-center'>
       <FontAwesomeIcon icon={faSpinner} spinPulse size='2xl' />
     </div>
   ) : (
     <div className='flex h-full w-full flex-col gap-3 overflow-auto p-2'>
-      <CategoryBtn name='Category nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' id='123' activeId={activeId} />
-      <CategoryBtn name='Category nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' id='123' activeId={activeId} />
-      <CategoryBtn name='Category nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' id='111' activeId={activeId} />
-      <CategoryBtn name='Category nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' id='123' activeId={activeId} />
-      <CategoryBtn name='Category nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' id='123' activeId={activeId} />
-      <CategoryBtn name='Category nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' id='123' activeId={activeId} />
-      <CategoryBtn name='Category nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' id='123' activeId={activeId} />
-      <CategoryBtn name='Category nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' id='123' activeId={activeId} />
-      <CategoryBtn name='Category nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' id='123' activeId={activeId} />
-      <CategoryBtn name='Category nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' id='123' activeId={activeId} />
-      <CategoryBtn name='Category nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' id='123' activeId={activeId} />
-      <CategoryBtn name='Category nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' id='123' activeId={activeId} />
-      <CategoryBtn name='Category nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' id='123' activeId={activeId} />
-      <CategoryBtn name='Category nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' id='123' activeId={activeId} />
-      <CategoryBtn name='Category nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' id='123' activeId={activeId} />
-      <CategoryBtn name='Category nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' id='123' activeId={activeId} />
-      <CategoryBtn name='Category nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' id='123' activeId={activeId} />
-      <CategoryBtn name='Category nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' id='123' activeId={activeId} />
-      <CategoryBtn name='Category nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' id='123' activeId={activeId} />
-      <CategoryBtn name='Category nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' id='123' activeId={activeId} />
-      <CategoryBtn name='Category nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' id='123' activeId={activeId} />
+      {categories.length > 0 &&
+        categories.map((category) => (
+          <CategoryBtn
+            key={category._id}
+            name={category.name}
+            id={category._id}
+            activeId={activeId}
+            setActiveId={setActiveId}
+          />
+        ))}
+      <CategoryBtnSkeleton />
     </div>
   );
 };
