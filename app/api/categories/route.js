74bca59a21800ac6aa6cf4cd9a81eb59bd2e3b8a
SnowSnail <f2961@yandex.ru>
2023-07-02 16:05:55 +0300

    Add api route for fetching all categories

---
+++ app/api/categories/route.js
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+import mongoose from 'mongoose';
+import Category from '@/models/Category';
+
+// Get all categories
+export async function GET(request) {
+  const db = mongoose.connect('mongodb://localhost/fav-plus-test-db');
+
+  const categories = await Category.find();
+
+  return NextResponse.json({ categories });
+}
+
+export const dynamic = 'force-dynamic';
